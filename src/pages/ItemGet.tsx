306c5d300220c0a4ee285b9ab4f2031e4ad8a351
changhak0310 <dlckdgkr0320@naver.com>
2023-08-22 08:31:20 +0900

    feat(ItemGet): 아이템 획득 시 인벤토리가 다 차있으면 alert 표기

--- src/pages/ItemGet.tsx
+++ src/pages/ItemGet.tsx
@@ -1,10 +1,12 @@
 /** @jsxImportSource @emotion/react */
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import styled from '@emotion/styled'
 import { css } from "@emotion/react"
 import { AddItemToInventory } from '.';
-import Item from '../json/DemoItem';
+import Item, { ItemProps }  from '../json/DemoItem';
+import { InventorySelectContext, AlertContext } from '../module/index';
+import { DeletItemToInventory } from './Inventory';
 
 interface OpacityProps {
     active: boolean;
@@ -16,31 +18,20 @@ interface Window {
         getItem: () => string | undefined;
         webViewIsVisible: () => void | undefined;
     }
-  }
-
-const ItemGetStyle = styled.div<OpacityProps>`
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    max-width: 412px;
-    max-height: 915px;
-    ${({active}) => 
-        active ?
-        css`opacity: 0;` :
-        css`opacity: 1;`
-    };
-`
-const Button = styled.button`
-    width: 100px;
-    height: 50px;
-    background-color: #ff4d4d;
-`
-
+}
 
 export const ItemGet = () => {
     const [item, setItem] = useState<string | null>(null);
     const [isOpacity, setIsOpacity] = useState(false);
+    const [userItem, setUserItem] = useState<ItemProps[]>(JSON.parse(localStorage.getItem('userData') || '[]').userItem);
 
+    const { alert } = useContext(AlertContext)
+    const { invenSelect } = useContext(InventorySelectContext)
+
+    const GetUserItem = () => {
+        const userDataItem = JSON.parse(localStorage.getItem('userData') || '[]').userItem;
+        setUserItem(userDataItem);
+    }
     const SendAndroidItem = () => {
         try{
             return window.Android?.getItem();
@@ -50,15 +41,39 @@ export const ItemGet = () => {
             return undefined;
         }
     }
+    const onAlertDeletItem = async (item: ItemProps[string]) => {
+        const AlertLeftButton = () => {
+            onInventorySelect(item);
+        }
+        const AlertRightButton = () => {
+            
+        }
+
+        const result = await alert(`가방에 ${item.name}을/를 넣을 자리가 없다.`, "가방 속 물건을 버린다.", `${item.name}을/를 버린다.`);
+        result ? AlertRightButton() : AlertLeftButton();
+    }
+    const onInventorySelect = async (item: ItemProps[string]) => {
+        const CancleToDeleteItem = () => {
+            onAlertDeletItem(item);
+        }
+        const DeleteSelectItem = () => {
+            const deleItem: ItemProps[string] = Item[userItem[result].name.toString()]
+            DeletItemToInventory(deleItem, result)
+            AddItemToInventory(item)
+        }
+        
+        const result = await invenSelect("취소", "버리기");
+        (result === -1) ? CancleToDeleteItem() : DeleteSelectItem();
+    }
+    
     const GetItems = () => {
         try{
-            const item = SendAndroidItem();
-            //const item = "도끼"
+            //const item = SendAndroidItem();
+            const item = "도끼"
             console.log(item+" 아이템을 얻었습니다.");
-            if (item !== null && item !== undefined) {
-                //const existingArray = JSON.parse(localStorage.getItem('item') || '[]');
-                //localStorage.setItem('item', JSON.stringify([...existingArray, item]));
-                //setItem(item)                
+            if(userItem.length >= 8) {
+                onAlertDeletItem(Item[item])
+            } else if (item !== null && item !== undefined) {              
                 AddItemToInventory(Item[item]);
                 setItem(item)
             }
@@ -70,7 +85,7 @@ export const ItemGet = () => {
 
     useEffect(() => {
         GetItems();
-        setIsOpacity(false)
+        setIsOpacity(false);
     }, [0])
 
     const onClickButton = () => {
@@ -94,3 +109,21 @@ export const ItemGet = () => {
         </ItemGetStyle>
     )
 }
+
+const ItemGetStyle = styled.div<OpacityProps>`
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    max-width: 412px;
+    max-height: 915px;
+    ${({active}) => 
+        active ?
+        css`opacity: 0;` :
+        css`opacity: 1;`
+    };
+`
+const Button = styled.button`
+    width: 100px;
+    height: 50px;
+    background-color: #ff4d4d;
+`
