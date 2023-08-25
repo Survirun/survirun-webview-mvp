8db5ee083a6fd8a5ed620986ef0aa3d97045363e
changhak0310 <dlckdgkr0320@naver.com>
2023-08-26 03:53:39 +0900

    fix(subCharacter): 서브 캐릭터 데이터 수정(조건 삭제, 스토리 제목 추가)

--- src/json/DemoSubCharacter.ts
+++ src/json/DemoSubCharacter.ts
@@ -9,7 +9,7 @@ interface OptionAddition {
         getOrLose: 'get' | 'lose',
         number: number
     }[] | null,
-    condition?: Condition,
+    condition?: Condition | null,
     nextProgress?: string | null,
     openStroy?: string | null,
     zombie?: number
@@ -22,12 +22,12 @@ interface Option {
 }
 
 interface SubCharaterStoryProps{
-    condition?: Condition,
+    storyTitle: string,
     storyText: string,
     option: Option[],
 }
 
-interface SubCharaterProps{
+interface SubCharacterProps{
     [key: string]: {
         name: string,
         subCharId: number,
@@ -42,7 +42,7 @@ interface SubCharaterProps{
     }   
 }
 
-const subCharater: SubCharaterProps = {
+const subCharacter: SubCharacterProps = {
     "테스트 캐릭터1": {
         name: "테스트 캐릭터1",
         subCharId: 1,
@@ -55,10 +55,7 @@ const subCharater: SubCharaterProps = {
         },
         story: [
             {
-                condition: {
-                    kind: "item",
-                    number: 1,
-                },
+                storyTitle: "테스트1",
                 storyText: "스토리1 테스트 테스트",
                 option: [
                     {
@@ -78,10 +75,7 @@ const subCharater: SubCharaterProps = {
                 ],
             },
             {
-                condition: {
-                    kind: "item",
-                    number: 2,
-                },
+                storyTitle: "테스트2",
                 storyText: "스토리2 테스트 테스트",
                 option: [
                     {
@@ -94,5 +88,71 @@ const subCharater: SubCharaterProps = {
                 ],
             }
         ]
+    },
+    "테스트 캐릭터2": {
+        name: "테스트 캐릭터2",
+        subCharId: 1,
+        img: "/subCarater2",
+        property: "압도",
+        charateristic: {
+            age: 10,
+            sex: "male",
+            backgroundSetting: "어쩌구 저쩌구 설명 설명2"
+        },
+        story: [
+            {
+                storyTitle: "2테스트 1",
+                storyText: "스토리1 테스트 테스트2",
+                option: [
+                    {
+                        optionID: "opt_1-1",
+                        text: "옵션1 테스트 테스트2",
+                        addition: {
+
+                        }
+                    },
+                    {
+                        optionID: "opt_1-2",
+                        text: "옵션1 테스트 테스트2",
+                        addition: {
+
+                        }
+                    }
+                ],
+            },
+            {
+                storyTitle: "2test 2",
+                storyText: "스토리2 테스트 테스트2",
+                option: [
+                    {
+                        optionID: "opt_2-1",
+                        text: "옵션2 테스트 테스트2",
+                        addition: {
+
+                        }
+                    }
+                ],
+            }
+        ]
     }
 }
+
+function updateAdditionProperties(subCharacter: SubCharacterProps): SubCharacterProps {
+    for (const charKey in subCharacter) {
+        const character = subCharacter[charKey];
+    
+        for (const story of character.story) {
+            for (const option of story.option) {
+                option.addition ??= {};
+                option.addition.result ??= null;
+                option.addition.condition ??= null;
+                option.addition.nextProgress ??= null;
+                option.addition.openStroy ??= null;
+            }
+        }
+    }
+    return subCharacter;
+}
+
+const updateSubCharacter = updateAdditionProperties(subCharacter);
+export const jsonSubCharacter = updateSubCharacter;
