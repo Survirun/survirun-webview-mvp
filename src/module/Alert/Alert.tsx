d6444af1c4f454b0c1a20f30f96e579f13ce6b0a
changhak0310 <dlckdgkr0320@naver.com>
2023-09-01 17:59:27 +0900

    feat: 폰트 디자인 추가

--- src/module/Alert/Alert.tsx
+++ src/module/Alert/Alert.tsx
@@ -1,17 +1,17 @@
 import { AlertState } from "./AlertDialog";
 
-export const Alert = ({message, leftText, rightText, onClickLeft, onClickRight}: AlertState) => {
+export const Alert = ({message, subMessage, leftText, rightText, onClickLeft, onClickRight}: AlertState) => {
     return(
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-10">
             <div className="fixed w-full h-full bg-black bg-opacity-30"></div>
             <div className="w-80 h-44 p-4 bg-white rounded-lg flex-col justify-start items-start gap-5 inline-flex z-[11]">
                 <div className="flex-col justify-start items-start gap-3 flex">
                     <div className="text-zinc-900 text-[17px] font-bold">{message}</div>
-                    <div className="text-gray-500 text-[14px] font-semibold">가방이 가득 찼어요.<br/>아이템을 버려야 해요.</div>
+                    <div className="text-gray-500 text-[14px] font-semibold whitespace-pre-line">{subMessage}</div>
                 </div>
                 <div className="w-72 justify-start items-start gap-2 inline-flex">
-                    <button onClick={onClickLeft} className="h-10 p-2 bg-gray-100 rounded-xl justify-center items-center flex text-zinc-900 text-[13px] font-semibold hover:opacity-80">{leftText}</button>
-                    <button onClick={onClickRight} className="h-10 p-2 bg-red-600 rounded-xl justify-center items-center flex text-white text-[13px] font-semibold hover:opacity-80">{rightText}</button>
+                    <button onClick={onClickLeft} className="w-36 h-10 p-2 bg-gray-100 rounded-xl justify-center items-center flex text-zinc-900 text-[13px] font-semibold hover:opacity-80">{leftText}</button>
+                    <button onClick={onClickRight} className="w-36 h-10 p-2 bg-red-600 rounded-xl justify-center items-center flex text-white text-[13px] font-semibold hover:opacity-80">{rightText}</button>
                 </div>
             </div>
         </div>
