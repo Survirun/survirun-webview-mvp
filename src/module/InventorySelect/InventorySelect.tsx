75c0e7463dcc1efbd032be7e7161567dc7dcb6b2
changhak0310 <dlckdgkr0320@naver.com>
2023-09-02 14:23:31 +0900

    feat(Item): 아이템 이미지 추가

--- src/module/InventorySelect/InventorySelect.tsx
+++ src/module/InventorySelect/InventorySelect.tsx
@@ -9,10 +9,10 @@ import { ItemProps } from "../../json/DemoItem";
 
 export const InventorySelect = ({leftText, rightText, onClickLeft, onClickRight}: InventorySelectProps) => {
     const [selectInvenItem, setSelectInvenItem] = useState<number>();
-    const [userItem, setUserItem] = useState<ItemProps>();
+    const [userItem, _] = useState<ItemProps>(JSON.parse(localStorage.getItem('userData') || '[]').userItem);
 
     useEffect(() => {
-        setUserItem(JSON.parse(localStorage.getItem('userData') || '[]').userItem)
+
     }, [])
 
     const HandleInventoryItemClick = (index: number) => {
@@ -28,24 +28,43 @@ export const InventorySelect = ({leftText, rightText, onClickLeft, onClickRight}
                 if(userItem[index] === undefined) {
                     return ""
                 }
-                return `${userItem[index].name}`
+                if(userItem[index].img === undefined) {
+                    return "/img/테스트.jpg"
+                }
+                return `${userItem[index].img}`
             } catch(err) {
                 console.error("Error: CreateInventoryName ", err);
             }
     }
     return(
-        <>
-            <Background/>
-            <CancleDeleteButton onClick={onClickLeft}>{leftText}</CancleDeleteButton>
-            <DeleteButton disabled={(selectInvenItem === undefined)} onClick={() => {if(selectInvenItem !== undefined) return onClickRight(selectInvenItem)}}>{rightText}</DeleteButton>
-            <InventoryStyle>
-            {Array.from({ length: 8 }).map((_, index) => (
-                <InventorySlot key={index}>
-                    <InventoryItem onClick={() => HandleInventoryItemClick(index)} css={(selectInvenItem === index) && inventorySelect}>{CreateInventoryName(index)}</InventoryItem>
-                </InventorySlot>
-            ))}  
-            </InventoryStyle>
-        </>
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-10">
+            <div className="fixed w-full h-full bg-black bg-opacity-30"></div>
+            <div className="flex items-center justify-center flex-col gap-3">
+                <div className="w-72 justify-center items-start gap-2 inline-flex">
+                    <button onClick={onClickLeft} className="w-36 h-10 p-2 bg-gray-100 rounded-xl justify-center items-center flex text-zinc-900 text-[13px] font-semibold hover:opacity-80 z-[11]">{leftText}</button>
+                    <button onClick={() => {if(selectInvenItem !== undefined) return onClickRight(selectInvenItem)}} className="w-36 h-10 p-2 bg-red-600 rounded-xl justify-center items-center flex text-white text-[13px] font-semibold hover:opacity-80 z-[11]">{rightText}</button>
+                </div>
+                <div className="w-[25rem] h-52 p-4 bg-white rounded-lg flex-wrap justify-center items-center gap-3 inline-flex z-[11]">
+                    {Array.from({ length: 8 }).map((_, index) => (
+                        <div onClick={() => HandleInventoryItemClick(index)} css={(selectInvenItem === index) && inventorySelect} key={index} className="w-20 h-20 bg-red-500 rounded-md border-2 border-solid border-neutral-500">
+                            <img src={CreateInventoryName(index)} className="w-full h-full rounded-md"></img>
+                        </div>
+                    ))} 
+                </div>
+            </div>
+        </div>
+        // <>
+        //     <Background/>
+        //     <CancleDeleteButton onClick={onClickLeft}>{leftText}</CancleDeleteButton>
+        //     <DeleteButton disabled={(selectInvenItem === undefined)} onClick={() => {if(selectInvenItem !== undefined) return onClickRight(selectInvenItem)}}>{rightText}</DeleteButton>
+        //     <InventoryStyle>
+        //     {Array.from({ length: 8 }).map((_, index) => (
+        //         <InventorySlot key={index}>
+        //             <InventoryItem onClick={() => HandleInventoryItemClick(index)} css={(selectInvenItem === index) && inventorySelect}>{CreateInventoryName(index)}</InventoryItem>
+        //         </InventorySlot>
+        //     ))}  
+        //     </InventoryStyle>
+        // </>
     )
 }
 
