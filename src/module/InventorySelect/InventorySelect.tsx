68437a75708f1b2862c914e73415860dab0a8493
changhak0310 <dlckdgkr0320@naver.com>
2023-09-02 14:25:09 +0900

    refactory(Select Item): tailwind로 변경

--- src/module/InventorySelect/InventorySelect.tsx
+++ src/module/InventorySelect/InventorySelect.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
 
-import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 import { useEffect, useState } from "react";
 
@@ -53,74 +52,9 @@ export const InventorySelect = ({leftText, rightText, onClickLeft, onClickRight}
                 </div>
             </div>
         </div>
-        // <>
-        //     <Background/>
-        //     <CancleDeleteButton onClick={onClickLeft}>{leftText}</CancleDeleteButton>
-        //     <DeleteButton disabled={(selectInvenItem === undefined)} onClick={() => {if(selectInvenItem !== undefined) return onClickRight(selectInvenItem)}}>{rightText}</DeleteButton>
-        //     <InventoryStyle>
-        //     {Array.from({ length: 8 }).map((_, index) => (
-        //         <InventorySlot key={index}>
-        //             <InventoryItem onClick={() => HandleInventoryItemClick(index)} css={(selectInvenItem === index) && inventorySelect}>{CreateInventoryName(index)}</InventoryItem>
-        //         </InventorySlot>
-        //     ))}  
-        //     </InventoryStyle>
-        // </>
     )
 }
 
-const Background = styled.div`
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
-    z-index: 10;
-`
-const CancleDeleteButton = styled.button`
-    position: absolute;
-    left: 30%;
-    top: 40%;
-    width: 80px;
-    height: 40px;
-    border-radius: 4px;
-    background-color: #eee;
-    font-size: 12px;
-    z-index: 11;
-`
-const DeleteButton = styled.button`
-    position: absolute;
-    left: 50%;
-    top: 40%;
-    width: 80px;
-    height: 40px;
-    border-radius: 4px;
-    background-color: #eee;
-    font-size: 12px;
-    z-index: 11;
-`
-const InventoryStyle = styled.div`
-    position: absolute;
-    top: 50%;
-    display: flex;
-    width: 100%;
-    margin: 0 auto;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #4f0b0b;
-    flex-wrap: wrap;
-    z-index: 11;
-`
-const InventorySlot = styled.div`
-    position: relative;
-    width: 25%;
-    padding: 10px;
-    box-sizing: border-box;
-`
-const InventoryItem = styled.div`
-    width: 100%;
-    height: 80px;
-    background-color: #ccc;
-    cursor: pointer;
-`
 const inventorySelect = css`
     border: 3px solid red;
 `
