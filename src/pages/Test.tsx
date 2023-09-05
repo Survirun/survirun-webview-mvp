8a1aed38ebf6c8b89a79a6f9ff3ace467ee16ea1
changhak0310 <dlckdgkr0320@naver.com>
2023-09-05 13:49:13 +0900

    test: 테스트 페이지 변경

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -5,8 +5,6 @@ interface Window {
   Android?: {
     showToast: (message: string) => void | undefined;
     getItem: () => void | undefined;
-    userHPUp: (num: number) => void | undefined;
-    userHPDown: (num: number) => void | undefined;
     userGetItem: (item: ItemProps[string]) => void | undefined;
     userLoseItem: (item: ItemProps[string]) => void | undefined;
   }
@@ -40,20 +38,6 @@ export const Test = () => {
     //   window.Android?.showToast(message);
     // };
 
-    const sendHPUpToAndroid = () => {
-      try {
-        window.Android?.userHPUp(10);   
-      } catch (error) {
-        console.error("Error: sendHPUpToAndroid"+error)
-      }
-    }
-    const sendHPDownToAndroid = () => {
-      try {
-        window.Android?.userHPDown(10);   
-      } catch (error) {
-        console.error("Error: sendHPDownToAndroid"+error)
-      }
-    }
     const sendGetItemToAndroid = () => {
       try {
         window.Android?.userGetItem(Item["도끼"]);   
@@ -89,8 +73,6 @@ export const Test = () => {
 
     return(
         <Frame>
-            <Button onClick={sendHPUpToAndroid}>HP 10 올리기</Button>
-            <Button onClick={sendHPDownToAndroid}>HP 10 내리기</Button>
             <Button onClick={sendGetItemToAndroid}>도끼 획득</Button>
             <Button onClick={sendLoseItemToAndroid}>도끼 삭제</Button>
         </Frame>
