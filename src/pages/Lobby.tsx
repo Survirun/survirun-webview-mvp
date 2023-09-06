24c50ad619d0056597a9281cc9057845f3b9806d
changhak0310 <dlckdgkr0320@naver.com>
2023-09-06 17:33:09 +0900

    feat(Lobby): 캐릭터 대사 랜덤화

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTypingEffect } from "../hooks"
 
 export const Lobby = () => {
     const [clickCharacter, setClickCharacter] = useState(false);
     
-    const messages = ["안녕하세요?", "다음에 봐요."];
+    const [messages, setMessage] = useState(["안녕하세요?", "다음에 봐요.", "훗"]);
     const { typedText, startTyping, nextMessage } = useTypingEffect(messages, 50);
 
     const handleClickCharacter = () => {
@@ -17,6 +17,16 @@ export const Lobby = () => {
             setClickCharacter(false);
         }
     }
+
+    useEffect(() => {
+        const message = [
+            ["안녕하세요?", "다음에 봐요.", "훗"],
+            ["뭘봐요?", "별꼴이야 정말."],
+            ["아 배고프다."]
+        ]
+        const number = Math.floor(Math.random() * message.length);
+        setMessage(message[number])
+    }, [])
 
     return (
         <div className="absolute w-screen h-screen bg-[url('https://i.pinimg.com/564x/54/59/cd/5459cdf629146ad9710fdd9dab0a562f.jpg')] bg-no-repeat bg-cover">
@@ -32,7 +42,7 @@ export const Lobby = () => {
                 {clickCharacter ? 
                     <div onClick={handleClickBG} className="absolute top-0 z-10 w-screen h-screen">
                     <div className="absolute bottom-0 w-screen h-[400px] bg-gradient-to-b from-transparent to-black"/>
-                    <div className="absolute bottom-0 h-[200px] p-6 text-white">
+                    <div className="w-full absolute bottom-0 h-[200px] p-6 text-white">
                             <p className="text-xl font-bold">테스트</p>
                             {typedText}
                         </div>
