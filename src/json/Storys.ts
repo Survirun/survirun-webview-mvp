3596f99dae43f7e35899c1acdc8f231f7e78e9cd
changhak0310 <dlckdgkr0320@naver.com>
2023-10-17 18:36:49 +0900

    refactory(Story): 스토리 데이터 옵션 인터페이스 나누기

--- src/json/Storys.ts
+++ src/json/Storys.ts
@@ -1,14 +1,15 @@
 import testdata from './testData.json'
 
+export interface OptionsInterface {
+  optionID: string;
+  optionText: string;
+}
 export interface StoryInterface {
     title: string;
     startID: string;
     progressStory: Record<string, {
       text: string;
-      options: {
-        optionID: string;
-        optionText: string;
-      }[];
+      options: OptionsInterface[];
     }>;
 }
 
