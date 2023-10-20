ab2a7e09a833e6f01c13a1c00111763d245e2242
changhak0310 <dlckdgkr0320@naver.com>
2023-10-21 05:55:07 +0900

    feat(Components): input,number시 value 타입 number로변경

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -2,21 +2,24 @@ interface InputProps {
     text: string | number;
     placeholder: string;
     type?: string;
-    setState: (id: string) => void;
+    setState: (id: string | number) => void;
 }
 
-const onChange = (e: React.ChangeEvent<HTMLInputElement>, setState: (id: string) => void) => {
-   setState(e.target.value);
-};
+const onChange = (e: React.ChangeEvent<HTMLInputElement>, setState: (id: string | number) => void, isNumber: boolean) => {
+    const value = isNumber ? parseFloat(e.target.value) : e.target.value;
+    setState(value);
+ };
   
- export const Input = ({ text, placeholder, setState }: InputProps) => {
+ export const Input = ({ text, placeholder, type="text", setState }: InputProps) => {
+    const isNumberInput = type === "number";
+
     return (
         <input
-          type="text"
+          type={type}
           className="w-full p-2 mb-4 border border-gray-300 rounded"
           placeholder={placeholder}
           value={text}
-          onChange={(e) => onChange(e, setState)}
+          onChange={(e) => onChange(e, setState, isNumberInput)}
         />
     );
 };
