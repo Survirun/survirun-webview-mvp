2e25992117fdb8501a252105643f2093c1e1461d
changhak0310 <dlckdgkr0320@naver.com>
2023-10-21 03:02:57 +0900

    refactory(Components): 코드 재사용 높이는 기능 추가 (커링)

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -1,24 +1,21 @@
-import { Dispatch } from 'react'
-import { Story } from '../pages/MakeStory';
 interface InputProps {
-    index: number;
     text: string;
     placeholder: string;
-    setState: (id: string, number?: number) => void
+    setState: (id: string) => void
 }
 
-const onChange = (index: number,  e: React.ChangeEvent<HTMLInputElement>, setState: (id: string, number?: number) => void) => {
-   setState(e.target.value, index);
+const onChange = (e: React.ChangeEvent<HTMLInputElement>, setState: (id: string, number?: number) => void) => {
+   setState(e.target.value);
 };
   
- export const Input = ({ index, text, placeholder, setState }: InputProps) => {
+ export const Input = ({ text, placeholder, setState }: InputProps) => {
     return (
         <input
           type="text"
           className="w-full p-2 mb-4 border border-gray-300 rounded"
           placeholder={placeholder}
           value={text}
-          onChange={(e) => onChange(index, e, setState)}
+          onChange={(e) => onChange(e, setState)}
         />
     );
 };
