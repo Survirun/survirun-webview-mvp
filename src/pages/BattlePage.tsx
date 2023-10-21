54123dd855c15d31fdfe103fcfe22c790c00a6f8
changhak0310 <dlckdgkr0320@naver.com>
2023-10-21 22:49:38 +0900

    test: 연결 테스트 중

--- src/pages/BattlePage.tsx
+++ src/pages/BattlePage.tsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react";
 export const BattlePage = () => {
     const [data, setData] = useState('');
     useEffect(() => {
+        console.log("dddsad");
         window.addEventListener('message', function(event) {
-            var receivedData = event.data.distance;
+            var receivedData = event.data;
             console.log('받은 메시지:', receivedData);
             setData(receivedData)
         });
