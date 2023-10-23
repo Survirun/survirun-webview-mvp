7d2eaab5d4a88deee42c32a2f41c884ec66d47fe
changhak0310 <dlckdgkr0320@naver.com>
2023-10-23 22:29:41 +0900

    test

--- src/pages/BattlePage.tsx
+++ src/pages/BattlePage.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 
 export const BattlePage = () => {
-    const [data, setData] = useState('');
+    const [data, setData] = useState<number>();
     useEffect(() => {
-        console.log("dddsad");
-        window.addEventListener('message', function(event) {
-            var receivedData = event.data;
-            console.log('받은 메시지:', receivedData);
-            setData(receivedData)
-        });
+        setInterval(() => {
+            window.addEventListener('message', (event) => {
+                console.log(event)
+                setData(event.data)
+                console.log('자식 페이지에서 메시지 받음: ' + event.data);
+            });
+        }, 100)
     },[])
+
+    
     
     return(
         <>data: {data}</>
