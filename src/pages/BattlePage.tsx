24d587bd3b60c340a9390d24ddf6d2973d8bc81d
changhak0310 <dlckdgkr0320@naver.com>
2023-10-24 03:46:19 +0900

    feat: progress 추가

--- src/pages/BattlePage.tsx
+++ src/pages/BattlePage.tsx
@@ -1,17 +1,31 @@
 import { useEffect, useState } from "react";
+import { UpProgress } from "../Components";
+
+function createBattle() {
+  return (
+    <>
+      <UpProgress width={50} height={400} max={100} value={80} color="#fd3232" bgColor="#fff"/>
+      <UpProgress width={100} height={400} max={100} value={80} color="#fd3232" bgColor="#fff"/>
+    </>
+  );
+}
 
 export const BattlePage = () => {
-    const [data, setData] = useState();
-    useEffect(() => {
-        window.addEventListener("message", function (e) {
-            alert(e.data.distance)
-            setData(e.data.distance);
-        });
-    }, []);
+  const [data, setData] = useState();
+  useEffect(() => {
+    window.addEventListener("message", function (e) {
+      setData(e.data.distance);
+    });
+  }, []);
 
-    
-    
-    return(
-        <>data: {data}</>
-    )
-} 
+  return (
+    <div className="flex flex-col w-screen h-screen">
+      <div className="flex-row w-full bg-black h-1/2">
+        {createBattle()}
+        </div>
+      <div className="flex-row-reverse w-full h-1/2 bg-slate-400">
+        {createBattle()}
+      </div>
+    </div>
+  );
+};
