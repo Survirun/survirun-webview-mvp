f10d0d6a11d6945f9187aa47fc6fb164d5b5e64f
changhak0310 <dlckdgkr0320@naver.com>
2023-10-23 23:35:09 +0900

    test: 연결 테스트 중

--- src/pages/BattlePage.tsx
+++ src/pages/BattlePage.tsx
@@ -4,8 +4,8 @@ export const BattlePage = () => {
     const [data, setData] = useState();
     useEffect(() => {
         window.addEventListener("message", function (e) {
-            console.log(e.data.distance)
-          setData(e.data.distance);
+            alert(e.data.distance)
+            setData(e.data.distance);
         });
     }, []);
 
